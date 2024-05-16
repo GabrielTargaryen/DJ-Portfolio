ec6e1d04d97749bdd57913ee758b0f2cdaa73455
GabrielTargaryen <rebeldegabriel8@gmail.com>
2024-05-16 08:49:19 -0500

    first commit

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -86,7 +86,11 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('postgres://dj_portfolio_jlpb_user:Q0IkUVQJr8STlkV4x3N6QbXKPwq8IxTj@dpg-cp2hj6mv3ddc73cme1dg-a.oregon-postgres.render.com/dj_portfolio_jlpb'))
+    'default': dj_database_url.config(
+    default=os.environ.get('postgres://dj_portfolio_jlpb_user:Q0IkUVQJr8STlkV4x3N6QbXKPwq8IxTj@dpg-cp2hj6mv3ddc73cme1dg-a.oregon-postgres.render.com/dj_portfolio_jlpb'),
+    conn_max_age=600
+    )
+
 }
 
 
